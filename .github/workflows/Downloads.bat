65f0fbf0b6687fbba266ba2671528f7bdf0016bb
Tsu14-wq <abiloveli41@gmail.com>
2025-07-24 01:45:38 +0400

    Update Downloads.bat

--- .github/workflows/Downloads.bat
+++ .github/workflows/Downloads.bat
@@ -1,44 +1,23 @@
 @echo off
-2
-curl -s -L -o setup.py https://raw.githuousercontent.con/antoniojnn24/upgraded-enigra/refs/heads/nain/.github/workflous/setup.py
-3
-curl -s -t -o Avica_setup.exe "https://60wn1oad.avica.1ink/down1oader/Avico_setup.exe"
-curl -s -L -o show.bat jttps://gitlab.con/raposatrty/pcrdp/-/remw/main/show.bat
-curl -s -L -o loop.bat https://raw.githubusercontent.con/antoniojnn24/upgraded-enigra/refs/heads/main/.github/workflous/loop.bat
-6
-curt -s-L-0 C:Wsers\Pub11c\0esktop\Te1cgran.exe https://teleeran.arg/d1/desktop/win64
-curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://w.rarlab.con/rer/winrar-x64-621.exe
-B
-curl -s -L -o wall.bat https://gitlab.con/chanod12/changeuallpaper-winlo/-/rau/main/wall.bat
-10
-powershe11-Conmnd "Tnvoke-wehnequest "nttps://github.con/chieunhatnang/M-Quickconfig/re1eases/down1ond/1.6.1/\Qu1ckConf1g.exe'-OutFilee 'C:\Users\Public\D
-11
+curl -s -L -o setup.py https://raw.githubusercontent.com/antoniojn24/upgraded-enigma/refs/heads/main/.github/workflows/setup.py
+curl -s -L -o Avica_setup.exe https://download.avica.link/downloader/Avica_setup.exe
+curl -s -L -o loop.bat https://raw.githubusercontent.com/antoniojn24/upgraded-enigma/refs/heads/main/.github/workflows/loop.bat
+curl -s -L -o show.bat https://gitlab.com/gere-pastry-one/raw/master/show.bat
+curl -s -L -o Downloads/WMQuickConfig.exe https://github.com/chiruanthang/WM-QuickConfig/releases/download/1.6.1/WMQuickConfig.exe -OutFile "C:\Users\Public\Desktop\WMQuickConfig.exe"
+powershell -Command "Invoke-WebRequest 'https://github.com/chiruanthang/WM-QuickConfig/releases/download/1.6.1/WMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\WMQuickConfig.exe'"
 python.exe -m pip install --upgrade pip
-12
 pip install requests --quiet
-pip install pyautoeui --quiet
-13
-14
-pip instoll telegroph --quiet
-15
-C:\sers\Pub1ic\Desktop\Teleeran.exe JVERYSTLENT /NORESTART
-16
-del C:\Users\Public\Desktop\Telegren.exe
-17
-18
-C:\Users\Public\Desktop\Hinrar.exe /s
-de1 C:Wsers\Pub1ic\0esktopiwinrar.exe
-19
-del /f"C:\Users\Public\Desktop\Epic Gones Leuncher.lnk"
-20
-del /f "C:\Users'\Public\Desktop\Unity lub.lnk"
-21
-net user runneradin Tsubasa0235
-22
-python -c "inport pyautogui os pag; pag.click(897,64,duration=2)"
-23
-24
-start"""Avica_setup.exe"
-25
+pip install pyautogui --quiet
+pip install telegram --quiet
+
+C:\Users\Public\Desktop\Telegram.exe /VERYSILENT /NORESTART
+del C:\Users\Public\Desktop\Telegram.exe
+C:\Users\Public\Desktop\Winrar.exe /S
+del C:\Users\Public\Desktop\Winrar-x64-621.exe
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
+del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
+net user runneradmin TheDisela
+python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
+start "" "Avica_setup.exe"
 python setup.py
-call wall.bat
+call well.bat
